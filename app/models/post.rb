c8e564b32435e8e3cceaba09053f888f731639b8
runpixelrun <justindavidson23@gmail.com>
2015-12-23 09:53:51 +0000

    Added then removed bootstrap and used own styles for index page and navbar

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,8 @@
 class Post < ActiveRecord::Base
     validates :image, presence: true
     
-    has_attached_file :image, styles: { :medium => "640x" }
+    has_attached_file :image, styles: { large: "600x", medium: "300x", thumb: "100x" }
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-    
-    
-   
-    
-    
-    
-    
     
 
 end
