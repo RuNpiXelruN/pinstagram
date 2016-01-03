c83b4b946de59fa6dd245cc36a4e77cb060e7de2
runpixelrun <justindavidson23@gmail.com>
2016-01-03 08:56:09 +0000

    Secured edit and delete functions and added users link to each post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :owned_post, only: [:edit, :update, :destroy]
     
     
     def index
@@ -8,11 +9,13 @@ class PostsController < ApplicationController
     end
     
     def new
-        @post = Post.new
+        @post = current_user.posts.build
     end
     
     def create
-        if @post = Post.create(post_params)
+        @post = current_user.posts.build(post_params)
+        
+        if @post.save
             flash[:success] = "Your post has been created!"
             redirect_to posts_path
         else
@@ -26,7 +29,7 @@ class PostsController < ApplicationController
     end
     
     def edit
-            #   being called in before_action above
+        #   being called in before_action above
     end
     
     def update
@@ -56,5 +59,12 @@ class PostsController < ApplicationController
     
     def set_post
        @post = Post.find(params[:id]) 
+    end
+    
+    def owned_post
+        unless current_user == @post.user
+            flash[:notice] = "That doesn't belong to you!"
+            redirect_to root_path
+        end
     end
 end
